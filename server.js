a1a05957153dfc5a5fd85a53c1e555374a27f7af
Wade Facer <wadepfacer@gmail.com>
2021-05-29 17:39:12 -0600

    added search by ID and search by behavior functionality to API

--- server.js
+++ server.js
@@ -36,12 +36,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const results = findById(req.params.id, animals);
+    if (results){
+        res.json(results);
+    }else{
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
